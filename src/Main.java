85d9aa0599b5fca5d508938813febefafc891187
artem <artemkbnv@yandex.ru>
2022-09-28 16:27:23 +0300

    сделал дз

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 18;
+        // задание 1
+        int age = 17;
+        if(age >= 18) {
+            System.out.println("Поздрравляем вас с совершенолетием");
+        } if ( age< 18) {
+            System.out.println("Нужно еще подождать до совершенолетия");
+        }
+        // задание 2
+        System.out.println("задание 2");
+        if(age >= 7 && age < 18){
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (age >= 18 && age <24){
+            System.out.println("Человек ходит в университет");
+        }
+        if ( age >= 24){
+            System.out.println("человек ходит на работу");
+        }
+        // задание 3
+         int capacity = 102;
+         int seatsCount = 60;
+         int otherCount = capacity - seatsCount;
+
+         int seatsUsed = 59;
+         int otherUsed = 41;
+         if (seatsUsed < seatsCount) {
+             System.out.println("Eсть еще " + (seatsCount + seatsUsed) + " сидячих мест");
+         }
+         if( otherUsed < otherCount) {
+             System.out.println("Есть еще " + (otherCount - otherUsed) + " стоячих мест");
+         }
 
     }
 }
